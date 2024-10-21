4ea3fd1545c6d508b743e0e7d4ef2b35ff229c95
Scarlett Luna <sluna6360@gmail.com>
2024-10-20 21:01:00 -0500

    Tab 2: add Scarlett Luna CV

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,20 +1,21 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @NgModule({
   imports: [
-    IonicModule,
     CommonModule,
     FormsModule,
+    IonicModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Permitir el uso de Web Components como ion-card-content
 })
 export class Tab2PageModule {}
+
